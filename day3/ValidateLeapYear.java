91bb4d70a94fc5cc3eee048ced0976c964feac07
Subodh Raturi <subodhblogger966@gmail.com>
2026-07-21 19:58:44 +0530

    day3 done

---
+++ day3/ValidateLeapYear.java
@@ -0,0 +1,19 @@
+package day3;
+
+import java.util.Scanner;
+
+public class ValidateLeapYear {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         
+        System.out.println("Enter the Year You want to check");
+        int y = sc.nextInt();
+        if (y % 400 == 0 || (y % 4 == 0 && y % 100 == 0)){
+            System.out.println(y + " " + " is a leap year");
+        }
+        else {
+            System.out.println(y + " " + " is not a leap year");
+        }
+    }
+}
